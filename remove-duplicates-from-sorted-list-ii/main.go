6d1eeca5133c1441a99a2011fd7aa81b049c054e
perrynzhou <perrynzhou@gmail.com>
2021-02-19 18:15:31 +0800

    update

--- remove-duplicates-from-sorted-list-ii/main.go
+++ remove-duplicates-from-sorted-list-ii/main.go
@@ -1,31 +1,54 @@
 package main
 //link :https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list-ii/
+import (
+	"fmt"
+)
 type ListNode struct {
 	Val  int
 	Next *ListNode
 }
 
-func deleteDuplicates(head *ListNode) *ListNode {
-   dict :=make(map[int]int)
-   tmp :=head
-   for {
-   	   if tmp ==nil {
-   	    	break
-	   }
-	   dict[tmp.Val]++
-	   tmp = tmp.Next
-   }
-   tmp = head
-   var prev *ListNode
-   for k,v :=range dict {
-   	if v==1 {
-   		tmp.Val=k
-   		prev = tmp
-   		tmp = tmp.Next
+func buildList(nums []int) *ListNode {
+	head := &ListNode{}
+	tail := head
+	for i := 0; i < len(nums); i++ {
+		newNode := &ListNode{}
+		newNode.Val = nums[i]
+		tail.Next = newNode
+		tail = newNode
 	}
+	return head.Next
+}
+func printLists(l *ListNode) {
+	for {
+		if l == nil {
+			fmt.Println(" ")
+			break
+		}
+		fmt.Printf("%d ", l.Val)
+		l = l.Next
+	}
+}
+func deleteDuplicates(head *ListNode) *ListNode {
+   cur :=head
+   for {
+        if cur == nil {
+        	break
+		}
+		next := cur.Next
+		if next !=nil {
+			if cur.Val == next.Val {
+				cur = next.Next
+			}else {
+				fmt.Println("value:",cur.Val)
+			}
+		}
+		cur = next
    }
-   if prev!=nil {
-	   prev.Next = nil
-   }
+
 	return head
+}
+func main() {
+  l1 := buildList([]int{1,2,2,3,3,5})
+	deleteDuplicates(l1)
 }
